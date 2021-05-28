3d1a071e3e3b1d1ac6ec1b08461a237bdd5249a8
peeraya <peeraya_boon@cmu.ac.th>
2021-05-28 17:05:44 +0700

    hw

--- 1.js
+++ 1.js
@@ -1,3 +1,36 @@
-document.getElementById('ok').addEventListener('click', function(e){
-    alert('ok is clicked')
+document.getElementById('ok').addEventListener('click', function(e)
+{
+    alert('ok')
+    console.log(e)
+})
+document.getElementById('cancle').addEventListener('click', function(e)
+{
+    alert('cancel')
+    console.log(e)
+})
+document.getElementById('no').addEventListener('click', function(e)
+{
+    alert('no')
+    console.log(e)
+})
+
+document.getElementById('cancle').addEventListener('mousemove',function(c){
+   var x = c.client;
+   var y = c.client;
+   console.log('x:'+x+','+'y:'+y)
 }
+)
+
+document.getElementById('ok').addEventListener('mousemove',function(c){
+    var x = c.client;
+    var y = c.client;
+    console.log('x:'+x+','+'y:'+y)
+ }
+ )
+
+ document.getElementById('no').addEventListener('mousemove',function(c){
+    var x = c.client;
+    var y = c.client;
+    console.log('x:'+x+','+'y:'+y)
+ }
+ )
